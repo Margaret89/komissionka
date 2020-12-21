a86c5c729b473e10aa838b1bb8b7e8f944cf141d
match14 <match14@yandex.ru>
2020-12-21 17:49:42 +0300

    action panel

--- conf/webpack.dev.conf.js
+++ conf/webpack.dev.conf.js
@@ -9,7 +9,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: baseWebpackConfig.externals.paths.dist,
-    port: 8081,
+    port: 8085,
     overlay: {
       warnings: true,
       errors: true
