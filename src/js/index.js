d11ffbce0f6f4bda40ec56c11becb755fd3bb4c8
Margaret <match14@yandex.ru>
2020-12-21 22:18:00 +0300

    added popups

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import {$} from './common';
+import {$, Inputmask} from './common';
 
 // $(window).scroll(function(){
 // 	if($(this).scrollTop()>300){
@@ -9,7 +9,7 @@ import {$} from './common';
 // });
 // $('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
 
-// top slider
+// Верхний слайдер
 if($('.js-top-slider').length){
 	$('.js-top-slider').slick({
 		vertical: true,
@@ -18,3 +18,35 @@ if($('.js-top-slider').length){
 		dots: true,
 	});
 }
+
+// Маска для телефона
+Inputmask('+7 (999) 999-99-99').mask('.js-phone');
+
+// Передача данных формы авторизации
+if($('.js-auth-form').length){
+	$('.js-auth-form form').on('submit',function(e){
+		var $inputPhone = $(this).find('.js-phone');
+		var valPhone = $inputPhone.val();
+
+		if(valPhone.indexOf('_') > 0 ){
+			$inputPhone.addClass('error');
+		} else {
+			$inputPhone.removeClass('error');
+			$('.js-auth-form').css('display', 'none');
+			$('.js-auth-confirm').find('.js-phone').val(valPhone);
+			$('.js-auth-confirm').css('display', 'block');
+		}
+
+		e.preventDefault();
+	});
+}
+
+$(".js-btn-auth-popup").fancybox({
+	'afterClose'  : function() {
+		console.log('22222');
+		$('.js-auth-confirm form')[0].reset();
+		$('.js-auth-form form')[0].reset();
+		$('.js-auth-confirm').css('display', 'none');
+		$('.js-auth-form').css('display', 'block');
+	}
+});
